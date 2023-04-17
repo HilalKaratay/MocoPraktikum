6fc4b5d927f80770143d4ecc6e393e315a87922b
HilalKaratay <56538589+HilalKaratay@users.noreply.github.com>
2023-04-17 13:46:53 +0200

    Erstes Hallo

--- app/src/main/java/com/example/mocopraktikum/MainActivity.kt
+++ app/src/main/java/com/example/mocopraktikum/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
     }
-
+ // HALLO MEIN NAME IST
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
